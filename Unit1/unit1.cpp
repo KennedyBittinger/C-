0f39a8b8f230a3de3de857e4477da1cb13afd863
Kennedy Bittinger <157091595+KennedyBittinger@users.noreply.github.com>
2024-02-27 22:24:02 -0500

    Add files via upload

---
+++ Unit1/unit1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "**********************************" << std::endl;
+    std::cout << "*    Programming Assignment 1    *" << std::endl;
+    std::cout << "*     Computer Programming I     *" << std::endl;
+    std::cout << "*    Author: Kennedy Bittinger   *" << std::endl;
+    std::cout << "*   Due Date: Thursday, Jan. 24  *" << std::endl;
+    std::cout << "**********************************" << std::endl;
+    return 0;
+}
